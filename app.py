f67c1ff10d9c01e0d8c4e72dd4858711fbc8f26e
zkzk8953 <aoao1023@gmail.com>
2023-02-10 14:11:33 +0900

    my commiy

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ card = card.Card()
 def main():
     return render_template('index.html')
 
-@app.route('/card', methods = ['GET', 'POST'])
+@app.route('/card', methods = ['GET', 'POST', 'PUT', 'DELETE'])
 def io_items(): 
     if request.method == 'GET':
         data = card.find({})
@@ -45,6 +45,42 @@ def io_items():
 
         card.create(doc)
         return jsonify({ 'status': 200, 'message': '추가되었습니다.' })
+    
+    elif request.method == 'PUT':
+        headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+
+        id_receive = request.form['id']
+        url_receive = request.form['url']
+        comment_receive = request.form['comment']
+
+        res = requests.get(url_receive, headers = headers)
+        soup = BeautifulSoup(res.text, 'html.parser')
+
+        image_org = soup.select_one('meta[property = "og:image"]')
+        title_org = soup.select_one('meta[property = "og:title"]')
+        description_org = soup.select_one('meta[property = "og:description"]')
+
+        url_img = image_org['content']
+        url_title = title_org['content']
+        url_description = description_org['content']
+
+        doc = {
+            'url': url_receive,
+            'comment': comment_receive,
+            'desc': url_description,
+            'image': url_img,
+            'title': url_title
+        }
+
+        card.update(id_receive, doc)
+        return jsonify({ 'status': 200, 'message': '수정되었습니다.' })
+
+    elif request.method == 'DELETE':
+        id_receive = request.form['id']
+
+        card.delete(id_receive)
+        return jsonify({ 'status': 200, 'message': '삭제되었습니다.' })
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port = 5000, debug = True)
--- static/js/modules/card.js
+++ static/js/modules/card.js
@@ -1,32 +1,60 @@
 const http = new HttpService("localhost/5000");
 
-let isVisible = false
+let isVisible = false;
+let editTargetId = "";
 
-function init() {
-    initEvents();
-    getCards();
+/**
+ * 기본 템플릿 render
+ */
+const render = () => {
+    let template = `
+    <div class="jumbotron">
+        <h1 class="display-4">나홀로 링크 메모장!</h1>
+        <p class="lead">중요한 링크를 저장해두고, 나중에 볼 수 있는 공간입니다.</p>
+        <hr class="my-4">
+        <button id="toggle" onclick="toggleBox()" class="btn btn-primary btn-sm" type="button">포스팅 박스 열기</button>
+    </div>
+    <div id="box" class="justify-content-center align-items-center d-flex"></div>
+    <div id="cards-box" class="card-columns mt-4"></div>
+    `;
+
+    $(".wrap").append(template);
 }
 
+/**
+ * 카드 목록 불러오기
+ */
 const getCards = async () => {
     const res = await http.request("/card", "GET");
 
     if(res.status == 200) {
-        res.data.forEach(card => {
-            $("#cards-box").append(`
-            <div class="card">
+        res.data.forEach((card, index) => {
+            let template = `
+            <div id="${card._id}" class="card">
                 <img src="${card.image}" class="card-img-top" alt="${card.title}">
                 <div class="card-body">
-                <h5 class="card-title">${card.title}</h5>
-                <p class="card-text">${card.desc}</p>
+                    <h5 class="card-title"><a class="text-warning" href="${card.url}">{{${card.title}}}</a></h5>
+                    <p class="card-text">${card.desc}</p>
+                    <div class=" text-right">
+                        <button type="button" onclick="toggleBox('true', '${card._id}', '${card.url}', '${card.comment}')" class="btn btn-primary">수정</button>
+                        <button type="button" onclick="deleteCards('${card._id}')" class="btn btn-primary">삭제</button>
+                    </div>
                 </div>
             </div>
-            `);
+            `;
+
+            $("#cards-box").append(template);
         });
     }else {
         alert("[error occured!!]", res)
     }
 }
 
+/**
+ * 새카드 추가
+ * @param {*} url 
+ * @param {*} comment 
+ */
 const postCards = async (url, comment) => {
     const res = await http.request("/card", "POST", { url: url, comment: comment });
 
@@ -39,42 +67,93 @@ const postCards = async (url, comment) => {
     }
 }
 
-const editCards = async (id, card) => {
-    const res = http.request("/card", "PUT");
+/**
+ * 카드 수정
+ * @param {*} id 
+ */
+const editCards = async (id, url, comment) => {
+    const res = await http.request("/card", "PUT", { id: id, url: url, comment: comment });
 
-    console.log(res);
+    if(res.status == 200) {
+        alert(res.message);
+        location.reload();
+    }else {
+        alert("에러 발생");
+        console.log(res);
+    }
 }
 
-const deleteCards = async () => {
-    const res = http.request("/card", "DELETE");
+/**
+ * 카드 삭제
+ */
+const deleteCards = async (id) => {
+    const res = await http.request("/card", "DELETE", { id: id });
 
-    console.log(res);
+    if(res.status == 200) {
+        alert(res.message);
+        location.reload();
+    }else {
+        alert("에러 발생");
+        console.log(res);
+    }
 }
 
-function toggleBox() {
+/**
+ * 포스팅박스 토글 
+ * @param {*} isEdit 
+ * @param {*} id 
+ */
+const toggleBox = (isEdit = false, id, url, comment) => {
+    let template = `
+    <div id="inputBox" class="w-50 p-3 border rounded">
+        <div class="form-group">
+            <label for="exampleInputEmail1">아티클 URL</label>
+            <input id="article" type="text" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">간단 코멘트</label>
+            <textarea id="comment" class="form-control"></textarea>
+        </div>
+        <button id="submitBtn" onclick="submit('${isEdit ? 'edit' : 'create'}')" type="button" class="btn btn-primary">${isEdit ? "수정" : "등록"}</button>
+    </div>
+    `;
+
     if(isVisible) {
-        $("#box").removeClass("d-flex").addClass("d-none");
+        $("#box").empty();
         $("#toggle").text("포스팅 박스 열기");
     }else {
-        $("#box").removeClass("d-none").addClass("d-flex");
+        $("#box").append(template);
         $("#toggle").text("포스팅 박스 닫기");
+
+        $("html, body").animate({ scrollTop: $("#box").offset().top }, 500);
+
+        if(isEdit) {
+            $("#article").val(url);
+            $("#comment").val(comment);
+            editTargetId = id;
+        }else {
+            $("#article").val("");
+            $("#comment").val("");
+        }
     }
 
-    $("#article").val("");
-    $("#comment").val("")
     isVisible = !isVisible;
 }
 
-function initEvents() {
-    $("#toggle").click(function() {
-        toggleBox();
-    });
+/**
+ * submit
+ * @param {*} type 
+ * @param {*} url 
+ * @param {*} comment 
+ */
+const submit = (type) => {
+    let url = $("#article").val();
+    let comment = $("#comment").val();
 
-    $("#submitBtn").click(function() {
-        let url = $("#article").val();
-        let comment = $("#comment").val();
+    if(type === "create") {
+        postCards(url, comment);
+    }else if(type === "edit") {
+        editCards(editTargetId, url, comment);
+    }
 
-        postCards(url, comment)
-        toggleBox();
-    });
 }
